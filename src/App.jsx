db2270db981f583b7cb3bd0a2c36ffab8d9a0916
Simba Ndewere <148736984+Simba-Ndewere@users.noreply.github.com>
2025-10-31 10:37:00 +0000

    add nav bar icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import navImg from './assets/list.png'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      
-    </>
+    <div className="container">
+      <div className="nav-bar">
+        <div className='nav-img'>
+          <img src={navImg} alt='nav bar icon'></img>
+        </div>
+        <h1>CV GENERATOR</h1>
+      </div>
+    </div>
   )
 }
 
