6dc4255a929ffbbc511a3ce27b4f36a303541088
Simba Ndewere <148736984+Simba-Ndewere@users.noreply.github.com>
2025-11-08 16:54:07 +0000

    add export to SlidingNav component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
-import './App.css'
+import './styles/App.css'
 import navImg from './assets/list.png'
+import SlidingNav from './components/SlidingNav.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,7 +10,7 @@ function App() {
     <div className="container">
       <div className="nav-bar">
         <div className='nav-img'>
-          <img src={navImg} alt='nav bar icon'></img>
+          <img src={navImg} alt='nav bar icon' onClick={SlidingNav}></img>
         </div>
         <h2>CV GENERATOR</h2>
       </div>
--- src/components/SlidingNav.jsx
+++ src/components/SlidingNav.jsx
@@ -1,7 +1,24 @@
-function SlidingNav(){
-    return(
-        <>
-        
-        </>
+import '../styles/SlidingNav.css'
+
+function SlidingNav() {
+
+    function closeNav() {
+
+    }
+
+    function openNav() {
+
+    }
+
+    return (
+        <div className="sideNav" id="mySideNav">
+            <a href="#" className="closebtn" >X</a>
+            <a href="#">About</a>
+            <a href="#">Services</a>
+            <a href="#">Clients</a>
+            <a href="#">Contact</a>
+        </div>
     )
 }
+
+export default SlidingNav;
