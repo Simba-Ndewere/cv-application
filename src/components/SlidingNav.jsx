972bde53c97b6e9f4509f564548eb1545d0142a3
Simba Ndewere <148736984+Simba-Ndewere@users.noreply.github.com>
2025-11-12 12:32:03 +0000

    create components sections for entering cv information

--- src/components/SlidingNav.jsx
+++ src/components/SlidingNav.jsx
@@ -1,15 +1,13 @@
 import '../styles/SlidingNav.css';
 import { useState } from 'react';
 
-function SlidingNav({widthValue, setWidth}) {
+function SlidingNav({ widthValue, setWidth }) {
 
     return (
-        <div className="sideNav" style={{width: widthValue}}>
-            <a href="#" className="closebtn" onClick={() => setWidth('0%')}>x</a>
-            <a href="#">About</a>
-            <a href="#">Services</a>
-            <a href="#">Clients</a>
-            <a href="#">Contact</a>
+        <div className="navContainer">
+            <div className="sideNav" style={{ width: widthValue }}>
+                <a href="#" className="closebtn" onClick={() => setWidth('0%')}>x</a>
+            </div>
         </div>
     )
 }
