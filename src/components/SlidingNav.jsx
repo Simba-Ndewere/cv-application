869ed6028eebdfa38970f53415a4e6f6549c332c
Simba Ndewere <148736984+Simba-Ndewere@users.noreply.github.com>
2025-11-13 15:23:17 +0000

    replace letter x from sliding nav with close icon

--- src/components/SlidingNav.jsx
+++ src/components/SlidingNav.jsx
@@ -1,12 +1,13 @@
 import '../styles/SlidingNav.css';
 import { useState } from 'react';
+import closeImg from '../assets/close.png'
 
 function SlidingNav({ widthValue, setWidth }) {
 
     return (
         <div className="navContainer">
             <div className="sideNav" style={{ width: widthValue }}>
-                <a href="#" className="closebtn" onClick={() => setWidth('0%')}>x</a>
+                <img src={closeImg} className="closebtn" alt='close nav icon' onClick={() => setWidth('0%')}></img>
             </div>
         </div>
     )
