57dbff77aef1953a641c79bd068ba51b2cadda22
Simba Ndewere <148736984+Simba-Ndewere@users.noreply.github.com>
2025-11-22 21:42:30 +0000

    design educational experience on input and output

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,9 @@ import WorkExperienceCV from './components/output/WorkExperienceCV.jsx';
 function App() {
 
   const [navWidth, setNavWidth] = useState('0%');
-  const [generalInformation, setGeneralInfo] = useState({ fullName: 'John Doe', email: 'johndoe@gmail.com', phoneNumber: '012456890', address: 'harrogate', personalStatement: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum' });
+  const [generalInformation, setGeneralInfo] = useState({ fullName: 'John Doe', email: 'johndoe@gmail.com', phoneNumber: '012456890', address: 'london', personalStatement: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum' });
   const [skills, setSkills] = useState([{ name: 'react', id: crypto.randomUUID() }, { name: 'nextjs', id: crypto.randomUUID() }, { name: 'springboot', id: crypto.randomUUID() }, { name: 'javascript', id: crypto.randomUUID() }, { name: 'java', id: crypto.randomUUID() }, { name: 'nodejs', id: crypto.randomUUID() }, { name: 'typescript', id: crypto.randomUUID() }]);
+  const [workExperience, setWorkExperience] = useState([{title: 'Senior Software Engineer', company: 'JP Morgan', id: crypto.randomUUID()}]);
 
   return (
     <>
--- src/components/input/WorkExperience.jsx
+++ src/components/input/WorkExperience.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import '../../styles/WorkExperience.css'
-function WorkExperience() {
+import deleteIcon from '../../assets/delete.png'
+
+function WorkExperience({ workExperience, setWorkExperience }) {
 
     const [collapsible, openCollapsible] = useState(false);
 
@@ -11,10 +13,34 @@ function WorkExperience() {
         <div className='general'>
             <button type="button" className={`collapsibleExperience ${collapsible ? 'active' : ''}`} onClick={() => collapsible ? openCollapsible(false) : openCollapsible(true)}>Work Experience</button>
             <div style={collapsible ? { display: "block" } : { display: "none" }} className='generalContainer'>
+                <div className="enteredExperience">
+                    <p className="titleExpe">Senior Software Engineer - </p>
+                    <p className="companyExpe">JP Morgan</p>
+                    <div>
+                        <img src={deleteIcon} img/>
+                    </div>
+                </div>
                 <form className='form' action={handleSubmit}>
                     <div className='form-group'>
                         <label>
-                            <input type='text' name="fullname" placeholder="Enter Work Experience" required />
+                            <input type='text' name="title" placeholder="Title" required />
+                        </label>
+                        <label>
+                            <input type='text' name="location" placeholder="Location" required />
+                        </label>
+                        <label>
+                            <input type='text' name="company" placeholder="Company" required />
+                        </label>
+                        <div className="year">
+                            <label for="yearStarted">From</label>
+                            <input type="date" name="yearStarted"></input>
+                        </div>
+                        <div className="year">
+                            <label for="yearEnd">To</label>
+                            <input type="date" name="yearEnded"></input>
+                        </div>
+                        <label>
+                            <textarea className="tasks" name="experienceTasks" placeholder="Tasks" required></textarea>
                         </label>
                         <button type="submit" className='submit'>
                             Submit
